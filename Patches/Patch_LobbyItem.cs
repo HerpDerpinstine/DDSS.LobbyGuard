92ae69640cb05b1c0ad42681d95d3ea4ef4e6e84
Herp Derpinstine <herpderpinstine@gmail.com>
2024-10-01 08:28:56 -0600

    Fixed an issue with the Lobby List Flooding patch not caching the proper address

--- Patches/Patch_LobbyItem.cs
+++ Patches/Patch_LobbyItem.cs
@@ -8,14 +8,20 @@ namespace DDSS_LobbyGuard.Patches
 {
     internal class Patch_LobbyItem
     {
-        internal static List<CSteamID> _lobbyList = new();
+        internal static Dictionary<string, CSteamID> _lobbyList = new();
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(LobbyItem), nameof(LobbyItem.SetLobby))]
         private static bool SetLobby_Prefix(LobbyItem __instance, CSteamID __0, ref string __1)
         {
-            // Check if Host already has a Lobby Shown
-            if (_lobbyList.Contains(__0))
+            // Get Lobby Host Address
+            string address = SteamMatchmaking.GetLobbyData(__0, "HostAdress");
+            if (string.IsNullOrEmpty(address)
+                || string.IsNullOrWhiteSpace(address))
+                return false;
+
+            // Check if Lobby is already in Cache
+            if (_lobbyList.ContainsKey(address))
             {
                 // Remove This Listing
                 GameObject.Destroy(__instance.gameObject);
@@ -25,7 +31,7 @@ namespace DDSS_LobbyGuard.Patches
             }
 
             // Add Lobby to Cache
-            _lobbyList.Add(__0);
+            _lobbyList[address] = __0;
 
             // Run Original
             return true;
