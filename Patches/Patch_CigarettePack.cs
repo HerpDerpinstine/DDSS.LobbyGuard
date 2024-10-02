9077938d15794d9891b11ae957c9a098708b5815
Herp Derpinstine <herpderpinstine@gmail.com>
2024-10-01 23:08:22 -0600

    Patched CigarettePack Manipulation

---
+++ Patches/Patch_CigarettePack.cs
@@ -0,0 +1,33 @@
+﻿using DDSS_LobbyGuard.Security;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppMirror;
+
+namespace DDSS_LobbyGuard.Patches
+{
+    internal class Patch_CigarettePack
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(CigarettePack), nameof(CigarettePack.InvokeUserCode_CmdSmokeCigarette__NetworkIdentity))]
+        private static bool InvokeUserCode_CmdSmokeCigarette__NetworkIdentity_Prefix(
+            NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get CigarettePack
+            CigarettePack cigarettePack = __0.TryCast<CigarettePack>();
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, cigarettePack.transform.position))
+                return false;
+
+            // Run Game Command
+            cigarettePack.UserCode_CmdSmokeCigarette__NetworkIdentity(sender);
+
+            // Prevent Original
+            return false;
+        }
+    }
+}
--- Patches/Patch_Usable.cs
+++ Patches/Patch_Usable.cs
@@ -9,28 +9,6 @@ namespace DDSS_LobbyGuard.Patches
     internal class Patch_Usable
     {
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(Usable), nameof(Usable.UserCode_CmdUseNoTypeVerification__NetworkIdentity))]
-        private static bool UserCode_CmdUseNoTypeVerification__NetworkIdentity_Prefix(
-            Usable __instance,
-            NetworkIdentity __0)
-        {
-            // Validate Player
-            if ((__instance.NetworkusingPlayer != null)
-                || (__0 == null))
-                return false;
-
-            // Get Player Controller
-            PlayerController player = __0.GetComponent<PlayerController>();
-
-            // Check Usables Count
-            if (player.currentUsables.Count < 0)
-                return false;
-
-            // Run Original
-            return true;
-        }
-
-        [HarmonyPrefix]
         [HarmonyPatch(typeof(Usable), nameof(Usable.InvokeUserCode_CmdUse__NetworkIdentity))]
         private static bool InvokeUserCode_CmdUse__NetworkIdentity_Prefix(
             NetworkBehaviour __0,
@@ -94,10 +72,6 @@ namespace DDSS_LobbyGuard.Patches
 
             // Get Player Controller
             PlayerController player = sender.GetComponent<PlayerController>();
-
-            // Check Usables Count
-            if (player.currentUsables.Count < 0)
-                return false;
 
             // Run Game Command
             usable.UserCode_CmdStopUse__NetworkIdentity(sender);
