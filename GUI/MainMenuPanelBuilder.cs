37dd754f834f717e6b1a73553091320249f556ae
Herp Derpinstine <herpderpinstine@gmail.com>
2024-10-24 19:25:08 -0600

    Update MainMenuPanelBuilder.cs

--- GUI/MainMenuPanelBuilder.cs
+++ GUI/MainMenuPanelBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Il2CppLocalization;
+﻿using Il2Cpp;
+using Il2CppLocalization;
 using Il2CppTMPro;
 using Il2CppUMUI;
 using Il2CppUMUI.UiElements;
@@ -38,6 +39,23 @@ namespace DDSS_LobbyGuard.GUI
             _newPanel.transform.localPosition = new(0.2f, 430.9f, 0f);
             _newPanel.name = Properties.BuildInfo.Name;
 
+            // Apply New Tab
+            UiTab newTab = _newPanel.GetComponent<UiTab>();
+            newTab.InitTab();
+            UIManager.instance.tabs[Properties.BuildInfo.Name] = newTab;
+            MenuTab[] mainMenuTabs = Resources.FindObjectsOfTypeAll<MenuTab>();
+            if (tabs.Length > 0)
+                foreach (MenuTab tab in mainMenuTabs)
+                {
+                    tab.childTabs.Add(newTab);
+                    break;
+                }
+
+            // Fix Panel Layering
+            int siblingIndex = aboutTab.transform.GetSiblingIndex() - 1;
+            if (siblingIndex < 0)
+                siblingIndex = 0;
+            _newPanel.transform.SetSiblingIndex(siblingIndex);
 
             // Get New Panel Title
             Transform titleTextTrans = _newPanel.transform.Find("NewsLetter/TopBar/Title");
