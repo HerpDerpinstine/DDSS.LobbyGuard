7ae618c7d4c8b792cfeb432ca4c89d7196f9bba0
Herp Derpinstine <herpderpinstine@gmail.com>
2024-10-26 22:43:01 -0600

    Update Patch_Usable.cs

--- Patches/Patch_Usable.cs
+++ Patches/Patch_Usable.cs
@@ -1,7 +1,11 @@
-﻿using DDSS_LobbyGuard.Security;
+﻿using DDSS_LobbyGuard.Config;
+using DDSS_LobbyGuard.Security;
 using HarmonyLib;
 using Il2Cpp;
+using Il2CppInterop.Runtime;
 using Il2CppMirror;
+using Il2CppProps.Door;
+using Il2CppProps.Scripts;
 
 namespace DDSS_LobbyGuard.Patches
 {
@@ -14,19 +18,58 @@ namespace DDSS_LobbyGuard.Patches
             NetworkBehaviour __0,
             NetworkConnectionToClient __2)
         {
-            // Check for Server
-            if (__2.identity.isServer)
-                return true;
-
             // Get Usable
             Usable usable = __0.TryCast<Usable>();
+            if ((usable == null)
+                || usable.WasCollected)
+                return false;
 
             // Get Sender
             NetworkIdentity sender = __2.identity;
 
             // Validate Distance
-            if (!InteractionSecurity.IsWithinRange(sender.transform.position, usable.transform.position))
+            if (!__2.identity.isServer
+                && !InteractionSecurity.IsWithinRange(sender.transform.position, usable.transform.position))
                 return false;
+
+            // Get Usable Type
+            Il2CppSystem.Type usableType = usable.GetIl2CppType();
+            if ((usableType == null)
+                || usableType.WasCollected)
+                return false;
+
+            // Check for StickyNoteController
+            if (usableType == Il2CppType.Of<StickyNoteController>())
+            {
+                // Get StickyNoteController
+                StickyNoteController stickyNote = usable.TryCast<StickyNoteController>();
+                if ((stickyNote == null)
+                    || stickyNote.WasCollected)
+                    return false;
+
+                // Get Holder
+                CollectibleHolder holder = stickyNote.currentHolder;
+                if ((holder == null)
+                    || holder.WasCollected)
+                    return false;
+
+                // Get DoorInteractable
+                DoorInteractable doorInteractable = holder.parentInteractable.TryCast<DoorInteractable>();
+                if ((doorInteractable == null)
+                    || doorInteractable.WasCollected)
+                    return false;
+
+                // Get DoorController
+                DoorController door = doorInteractable.doorController;
+                if ((door == null)
+                    || door.WasCollected)
+                    return false;
+
+                // Validate Door State
+                if (!ConfigHandler.Moderation.StickyNotesOnOpenDoors.Value
+                    && (door.state != 0))
+                    return false;
+            }
 
             // Run Game Command
             usable.UserCode_CmdUse__NetworkIdentity__NetworkConnectionToClient(sender, __2);
@@ -47,6 +90,9 @@ namespace DDSS_LobbyGuard.Patches
 
             // Get Usable
             Usable usable = __0.TryCast<Usable>();
+            if ((usable == null)
+                || usable.WasCollected)
+                return false;
 
             // Get Sender
             NetworkIdentity sender = __2.identity;
@@ -74,6 +120,9 @@ namespace DDSS_LobbyGuard.Patches
 
             // Get Usable
             Usable usable = __0.TryCast<Usable>();
+            if ((usable == null)
+                || usable.WasCollected)
+                return false;
 
             // Get Sender
             NetworkIdentity sender = __2.identity;
