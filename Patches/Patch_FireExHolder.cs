df552cc74dc4e38ceb683b33548c0d16d56fecd2
Herp Derpinstine <herpderpinstine@gmail.com>
2024-10-28 20:27:12 -0600

    Fixed an issue with FireExHolder not spawning the Fire Extinguisher when joining Lobbies

--- Patches/Patch_FireExHolder.cs
+++ Patches/Patch_FireExHolder.cs
@@ -11,7 +11,11 @@ namespace DDSS_LobbyGuard.Patches
         [HarmonyPatch(typeof(FireExHolder), nameof(FireExHolder.Start))]
         private static bool Start_Prefix(FireExHolder __instance)
         {
-            // Key Security
+            // Check for Server
+            if (!__instance.isServer)
+                return true;
+
+            // Fire Extinguisher Security
             FireExSecurity.SpawnFireEx(__instance);
 
             // Prevent Original
