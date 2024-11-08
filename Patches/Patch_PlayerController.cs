90a26002d23dd4cdbcbdac3d0be3b5654636190d
Herp Derpinstine <herpderpinstine@gmail.com>
2024-11-07 22:02:22 -0600

    Implemented Velocity Enforcement

--- Patches/Patch_PlayerController.cs
+++ Patches/Patch_PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using DDSS_LobbyGuard.Security;
 using HarmonyLib;
+using Il2Cpp;
 using Il2CppGameManagement;
 using Il2CppGameManagement.StateMachine;
 using Il2CppMirror;
@@ -50,6 +51,79 @@ namespace DDSS_LobbyGuard.Patches
 
             // Prevent Original
             return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerController), nameof(PlayerController.SetLocalVelocity))]
+        private static void SetLocalVelocity_Prefix(PlayerController __instance)
+        {
+            // Check for Chair
+            if (!NetworkServer.activeHost
+                || !__instance.isServer
+                || __instance.isInChair)
+                return;
+
+            // Check for Game Start
+            if ((GameManager.instance.NetworktargetGameState != (int)GameStates.InGame)
+                && (GameManager.instance.NetworktargetGameState != (int)GameStates.Meeting))
+                return;
+
+            // Check Sprint
+            bool isSprinting = __instance.NetworktargetState == (int)States.Sprint;
+            bool hasCoffeeEffect = !GameManager.instance.noSprinting
+                || (__instance.GetComponent<PlayerEffectController>().NetworktargetState == (int)PlayerEffects.SpeedBoost);
+
+            // Get Max Speed
+            float maxSpeed = ((isSprinting && hasCoffeeEffect)
+                ? __instance.sprintSpeed
+                : __instance.moveSpeed) * Time.unscaledDeltaTime;
+
+            // Validate Speed
+            Vector3 velocity = (__instance.transform.position - __instance.lastPos);
+            Vector3 velocityNoUp = velocity;
+            velocityNoUp.y = 0f;
+            float magnitude = velocityNoUp.magnitude;
+            if (magnitude <= maxSpeed)
+                return;
+
+            Vector3 newDistance = Vector3.ClampMagnitude(velocityNoUp, maxSpeed);
+            newDistance.y = velocity.y;
+
+            Vector3 correctedPos = __instance.lastPos + newDistance;
+            ForcePosition(__instance, correctedPos);
+        }
+
+        private static void ForcePosition(PlayerController __instance, Vector3 pos)
+        {
+            __instance.enabled = false;
+            __instance.netIdentity.enabled = false;
+
+            if (__instance.isLocalPlayer)
+                __instance.controller.enabled = false;
+
+            NetworkTransform trans = __instance.gameObject.GetComponent<NetworkTransform>();
+            if (trans != null)
+                trans.enabled = false;
+
+            __instance.transform.position = pos;
+            __instance.SetDirty();
+
+            if (trans != null)
+            {
+                trans.UserCode_CmdTeleport__Vector3(pos);
+                trans.SetPosition(pos);
+                trans.SetDirty();
+                trans.enabled = true;
+            }
+
+            if (__instance.isLocalPlayer)
+                __instance.controller.enabled = true;
+
+            __instance.netIdentity.enabled = true;
+            __instance.enabled = true;
+
+            if (__instance.NetworktargetState == (int)States.Idle)
+                __instance.NetworktargetState = (int)States.Movement;
         }
     }
 }
