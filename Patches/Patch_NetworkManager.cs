71b0bcea7036ba07e3c7efe7647e0eb57f469c0b
Herp Derpinstine <herpderpinstine@gmail.com>
2024-11-09 09:01:22 -0600

    Revert "Improved Invite Code Generation"
    This reverts commit bfe5fa5a31aa6ec5e9a09ab2a3f05194bedbc511.

---
+++ Patches/Patch_NetworkManager.cs
@@ -0,0 +1,29 @@
+﻿using DDSS_LobbyGuard.Config;
+using DDSS_LobbyGuard.Security;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppMirror;
+
+namespace DDSS_LobbyGuard.Patches
+{
+    [HarmonyPatch]
+    internal class Patch_NetworkManager
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.StartHost))]
+        private static void StartHost_Prefix()
+        {
+            // Check if the setting is on
+            if (!ConfigHandler.General.ExtendedInviteCodes.Value)
+                return;
+
+            // Check if already has Invite Code
+            if (!string.IsNullOrEmpty(SteamLobby.requestedLobbyCode)
+                && !string.IsNullOrWhiteSpace(SteamLobby.requestedLobbyCode))
+                return;
+
+            // Generate New Code
+            SteamLobby.requestedLobbyCode = InviteCodeSecurity.GenerateNewCode();
+        }
+    }
+}
