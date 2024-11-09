68ce35eea5e23258a0e2e7270214e4df852f3b08
Herp Derpinstine <herpderpinstine@gmail.com>
2024-11-09 04:23:22 -0600

    Cleanup

--- Patches/Patch_PlayerDetectionVolume.cs
+++ Patches/Patch_PlayerDetectionVolume.cs
@@ -1,5 +1,6 @@
 ﻿using DDSS_LobbyGuard.Security;
 using HarmonyLib;
+using Il2CppMirror;
 using Il2CppProps.Door;
 using UnityEngine;
 
@@ -12,6 +13,9 @@ namespace DDSS_LobbyGuard.Patches
         [HarmonyPatch(typeof(PlayerDetectionVolume), nameof(PlayerDetectionVolume.OnTriggerEnter))]
         private static bool OnTriggerEnter_Prefix(PlayerDetectionVolume __instance, Collider __0)
         {
+            if (!NetworkServer.activeHost)
+                return true;
+
             if (!PlayerTriggerSecurity.IsColliderValid(__0))
                 return false;
 
@@ -26,6 +30,9 @@ namespace DDSS_LobbyGuard.Patches
         [HarmonyPatch(typeof(PlayerDetectionVolume), nameof(PlayerDetectionVolume.OnTriggerExit))]
         private static bool OnTriggerExit_Prefix(PlayerDetectionVolume __instance, Collider __0)
         {
+            if (!NetworkServer.activeHost)
+                return true;
+
             // Prevent Original
             return false;
         }
