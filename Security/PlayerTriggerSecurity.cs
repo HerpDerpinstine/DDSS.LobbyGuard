dbdffbc0fe3742a8a573fb4627a2912a6671b476
Herp Derpinstine <herpderpinstine@gmail.com>
2024-11-14 01:25:56 -0600

    Fixed an issue with PlayerTriggerSecurity spamming errors

--- Security/PlayerTriggerSecurity.cs
+++ Security/PlayerTriggerSecurity.cs
@@ -14,38 +14,70 @@ namespace DDSS_LobbyGuard.Security
             => _playerCounts.Clear();
 
         internal static bool IsColliderValid(Collider other)
-            => ((other.gameObject.layer != 11) && (other.gameObject.layer != 13));
+            => ((other != null)
+                && !other.WasCollected
+                && (other.gameObject.layer != 11) 
+                && (other.gameObject.layer != 13));
 
         internal static void OnEnter(PlayerDetectionVolume trigger, Collider other)
         {
+            if ((trigger == null)
+                || trigger.WasCollected
+                || (other == null)
+                || other.WasCollected)
+                return;
+            
             int count = 0;
             _playerCounts.TryGetValue(trigger, out count);
+
             if (count <= 0)
                 count = 0;
+
             _playerCounts[trigger] = count++;
+
             trigger.isPlayerInside = true;
+
             trigger.StartCoroutine(ColliderCheckCoroutine(trigger, other));
         }
 
         private static IEnumerator ColliderCheckCoroutine(PlayerDetectionVolume trigger, Collider other)
         {
-            Collider triggerCollider = trigger.GetComponent<Collider>();
-
-            while (other.bounds.Contains(triggerCollider.bounds.min)
-                || other.bounds.Contains(triggerCollider.bounds.max)
-                || triggerCollider.bounds.Contains(other.bounds.min)
-                || triggerCollider.bounds.Contains(other.bounds.max))
-                yield return new WaitForSeconds(1f);
-
-            trigger.OnPlayerExit.Invoke();
-
-            int count = 0;
-            _playerCounts.TryGetValue(trigger, out count);
-            _playerCounts[trigger] = count--;
-            if (count <= 0)
+            if ((trigger == null)
+                || trigger.WasCollected
+                || (other == null)
+                || other.WasCollected)
+                yield break;
+            else
             {
-                count = 0;
-                trigger.isPlayerInside = false;
+                Collider triggerCollider = trigger.GetComponent<Collider>();
+
+                while ((other != null)
+                    && !other.WasCollected
+                    && (triggerCollider != null)
+                    && !triggerCollider.WasCollected
+                    && (other.bounds.Contains(triggerCollider.bounds.min)
+                        || other.bounds.Contains(triggerCollider.bounds.max)
+                        || triggerCollider.bounds.Contains(other.bounds.min)
+                        || triggerCollider.bounds.Contains(other.bounds.max)))
+                    yield return new WaitForSeconds(1f);
+
+                if ((trigger == null)
+                    || trigger.WasCollected)
+                    yield break;
+                else
+                {
+                    trigger.OnPlayerExit.Invoke();
+
+                    int count = 0;
+                    _playerCounts.TryGetValue(trigger, out count);
+                    _playerCounts[trigger] = count--;
+
+                    if (count <= 0)
+                    {
+                        count = 0;
+                        trigger.isPlayerInside = false;
+                    }
+                }
             }
         }
     }
