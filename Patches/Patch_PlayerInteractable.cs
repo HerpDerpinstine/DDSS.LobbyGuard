5a02188b0968d7c892a563cdd8825317dad670a8
Herp Derpinstine <herpderpinstine@gmail.com>
2024-12-19 03:37:44 -0600

    Implemented PlayerInteractable Security

---
+++ Patches/Patch_PlayerInteractable.cs
@@ -0,0 +1,185 @@
+﻿using DDSS_LobbyGuard.Security;
+using DDSS_LobbyGuard.Utils;
+using HarmonyLib;
+using Il2CppMirror;
+using Il2CppPlayer.Lobby;
+using Il2CppPlayer.Scripts;
+
+namespace DDSS_LobbyGuard.Patches
+{
+    [HarmonyPatch]
+    internal class Patch_PlayerInteractable
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerInteractable), nameof(PlayerInteractable.InvokeUserCode_PromoteToAssistant__NetworkIdentity__NetworkConnectionToClient))]
+        private static bool InvokeUserCode_PromoteToAssistant__NetworkIdentity__NetworkConnectionToClient_Prefix(NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get PlayerInteractable
+            PlayerInteractable interact = __0.TryCast<PlayerInteractable>();
+            if ((interact == null)
+                || interact.WasCollected)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+            if (sender.GetPlayerRole() != PlayerRole.Manager)
+                return false;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, interact.transform.position))
+                return false;
+
+            interact.UserCode_PromoteToAssistant__NetworkIdentity__NetworkConnectionToClient(sender, __2);
+
+            // Prevent Original
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerInteractable), nameof(PlayerInteractable.InvokeUserCode_DemoteFromAssistant__NetworkIdentity__NetworkConnectionToClient))]
+        private static bool InvokeUserCode_DemoteFromAssistant__NetworkIdentity__NetworkConnectionToClient_Prefix(NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get PlayerInteractable
+            PlayerInteractable interact = __0.TryCast<PlayerInteractable>();
+            if ((interact == null)
+                || interact.WasCollected)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+            if (sender.GetPlayerRole() != PlayerRole.Manager)
+                return false;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, interact.transform.position))
+                return false;
+
+            interact.UserCode_DemoteFromAssistant__NetworkIdentity__NetworkConnectionToClient(sender, __2);
+
+            // Prevent Original
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerInteractable), nameof(PlayerInteractable.InvokeUserCode_AcceptHandShake__NetworkIdentity__NetworkConnectionToClient))]
+        private static bool InvokeUserCode_AcceptHandShake__NetworkIdentity__NetworkConnectionToClient_Prefix(NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get PlayerInteractable
+            PlayerInteractable interact = __0.TryCast<PlayerInteractable>();
+            if ((interact == null)
+                || interact.WasCollected)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, interact.transform.position))
+                return false;
+
+            interact.UserCode_AcceptHandShake__NetworkIdentity__NetworkConnectionToClient(sender, __2);
+
+            // Prevent Original
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerInteractable), nameof(PlayerInteractable.InvokeUserCode_CmdRequestHandShake__NetworkIdentity))]
+        private static bool InvokeUserCode_CmdRequestHandShake__NetworkIdentity_Prefix(NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get PlayerInteractable
+            PlayerInteractable interact = __0.TryCast<PlayerInteractable>();
+            if ((interact == null)
+                || interact.WasCollected)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, interact.transform.position))
+                return false;
+
+            interact.UserCode_CmdRequestHandShake__NetworkIdentity(sender);
+
+            // Prevent Original
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerInteractable), nameof(PlayerInteractable.InvokeUserCode_CmdResetHandShake))]
+        private static bool InvokeUserCode_CmdResetHandShake_Prefix(NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get PlayerInteractable
+            PlayerInteractable interact = __0.TryCast<PlayerInteractable>();
+            if ((interact == null)
+                || interact.WasCollected)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, interact.transform.position))
+                return false;
+
+            interact.UserCode_CmdResetHandShake();
+
+            // Prevent Original
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerInteractable), nameof(PlayerInteractable.InvokeUserCode_CmdResetHandShakeRequest__NetworkIdentity))]
+        private static bool InvokeUserCode_CmdResetHandShakeRequest__NetworkIdentity_Prefix(NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get PlayerInteractable
+            PlayerInteractable interact = __0.TryCast<PlayerInteractable>();
+            if ((interact == null)
+                || interact.WasCollected)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, interact.transform.position))
+                return false;
+
+            interact.UserCode_CmdResetHandShakeRequest__NetworkIdentity(sender);
+
+            // Prevent Original
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PlayerInteractable), nameof(PlayerInteractable.InvokeUserCode_CmdResetOutgoingHandShake))]
+        private static bool InvokeUserCode_CmdResetOutgoingHandShake_Prefix(NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get PlayerInteractable
+            PlayerInteractable interact = __0.TryCast<PlayerInteractable>();
+            if ((interact == null)
+                || interact.WasCollected)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, interact.transform.position))
+                return false;
+
+            interact.UserCode_CmdResetOutgoingHandShake();
+
+            // Prevent Original
+            return false;
+        }
+    }
+}
---
+++ Patches/Patch_TaskController.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Il2CppPlayer.Tasks;
+
+namespace DDSS_LobbyGuard.Patches
+{
+    [HarmonyPatch]
+    internal class Patch_TaskController
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(TaskController), nameof(TaskController.InvokeUserCode_CmdInstantiateAndAssignSecretObjective__Int32__NetworkIdentity__NetworkIdentity))]
+        private static bool InvokeUserCode_CmdInstantiateAndAssignSecretObjective__Int32__NetworkIdentity__NetworkIdentity_Prefix()
+        {
+            // Prevent Original
+            return false;
+        }
+    }
+}
