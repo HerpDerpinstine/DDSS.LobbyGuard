334b324062eb105d09c7aa1bb19625b62c33f834
Herp Derpinstine <herpderpinstine@gmail.com>
2024-12-19 01:58:46 -0600

    Improved PaperShredder Security

--- Patches/Patch_PaperShredder.cs
+++ Patches/Patch_PaperShredder.cs
@@ -1,4 +1,5 @@
 ﻿using DDSS_LobbyGuard.Security;
+using DDSS_LobbyGuard.Utils;
 using HarmonyLib;
 using Il2CppMirror;
 using Il2CppProps.PaperShredder;
@@ -16,6 +17,10 @@ namespace DDSS_LobbyGuard.Patches
         {
             // Get PaperShredder ~ CURSE YOUR TURTLES!!!
             PaperShredder shredder = __0.TryCast<PaperShredder>();
+            if ((shredder == null)
+                || shredder.WasCollected
+                || shredder.collectibles.Count <= 0)
+                return false;
 
             // Get Sender
             NetworkIdentity sender = __2.identity;
@@ -26,6 +31,64 @@ namespace DDSS_LobbyGuard.Patches
 
             // Run Game Command
             shredder.UserCode_CmdStartInteraction__NetworkIdentity(sender);
+
+            // Prevent Original
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PaperShredder), nameof(PaperShredder.InvokeUserCode_TimerCallback__Single))]
+        private static bool InvokeUserCode_TimerCallback__Single_Prefix(
+           NetworkBehaviour __0,
+           NetworkReader __1,
+           NetworkConnectionToClient __2)
+        {
+            // Get PaperShredder ~ CURSE YOUR TURTLES!!!
+            PaperShredder shredder = __0.TryCast<PaperShredder>();
+            if ((shredder == null)
+                || shredder.WasCollected
+                || shredder.collectibles.Count <= 0)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, shredder.transform.position))
+                return false;
+
+            // Read Value
+            float timer = __1.SafeReadFloat();
+
+            // Run Game Command
+            shredder.UserCode_TimerCallback__Single(timer);
+
+            // Prevent Original
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PaperShredder), nameof(PaperShredder.InvokeUserCode_ShredDocument__NetworkIdentity__NetworkConnectionToClient))]
+        private static bool InvokeUserCode_ShredDocument__NetworkIdentity__NetworkConnectionToClient_Prefix(
+           NetworkBehaviour __0,
+           NetworkConnectionToClient __2)
+        {
+            // Get PaperShredder ~ CURSE YOUR TURTLES!!!
+            PaperShredder shredder = __0.TryCast<PaperShredder>();
+            if ((shredder == null)
+                || shredder.WasCollected
+                || shredder.collectibles.Count <= 0)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, shredder.transform.position))
+                return false;
+
+            // Run Game Command
+            shredder.UserCode_ShredDocument__NetworkIdentity__NetworkConnectionToClient(sender, __2);
 
             // Prevent Original
             return false;
