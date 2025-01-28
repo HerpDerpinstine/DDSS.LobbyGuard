7452b4f2d93089a71e6f25248053013befc29ca4
Herp Derpinstine <herpderpinstine@gmail.com>
2025-01-28 04:47:47 -0600

    Update Patch_PaperShredder.cs

--- Patches/Patch_PaperShredder.cs
+++ Patches/Patch_PaperShredder.cs
@@ -39,40 +39,6 @@ namespace DDSS_LobbyGuard.Patches
         }
 
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(PaperShredder), nameof(PaperShredder.InvokeUserCode_CmdSetInteractionTimeCounter__NetworkIdentity__Single__Boolean__NetworkConnectionToClient))]
-        private static bool InvokeUserCode_CmdSetInteractionTimeCounter__NetworkIdentity__Single__Boolean__NetworkConnectionToClient_Prefix(
-           NetworkBehaviour __0,
-           NetworkReader __1,
-           NetworkConnectionToClient __2)
-        {
-            // Get PaperShredder ~ CURSE YOUR TURTLES!!!
-            PaperShredder shredder = __0.TryCast<PaperShredder>();
-            if ((shredder == null)
-                || shredder.WasCollected
-                || shredder.collectibles.Count <= 0)
-                return false;
-
-            // Get Sender
-            NetworkIdentity sender = __2.identity;
-
-            // Validate Distance
-            if (sender.IsGhost()
-                || !InteractionSecurity.IsWithinRange(sender.transform.position, shredder.transform.position))
-                return false;
-
-            // Read Value
-            __1.SafeReadNetworkIdentity();
-            float timer = __1.SafeReadFloat();
-            bool initial = __1.SafeReadBool();
-
-            // Run Game Command
-            shredder.UserCode_CmdSetInteractionTimeCounter__NetworkIdentity__Single__Boolean__NetworkConnectionToClient(sender, timer, initial, __2);
-
-            // Prevent Original
-            return false;
-        }
-
-        [HarmonyPrefix]
         [HarmonyPatch(typeof(PaperShredder), nameof(PaperShredder.InvokeUserCode_ShredDocument__NetworkIdentity__NetworkConnectionToClient))]
         private static bool InvokeUserCode_ShredDocument__NetworkIdentity__NetworkConnectionToClient_Prefix(
            NetworkBehaviour __0,
