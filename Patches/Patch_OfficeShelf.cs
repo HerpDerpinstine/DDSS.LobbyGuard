77ba1cc72022c88f6f132040ddb523153aaf6939
Herp Derpinstine <herpderpinstine@gmail.com>
2025-02-01 07:30:08 -0600

    Update Patch_OfficeShelf.cs

--- Patches/Patch_OfficeShelf.cs
+++ Patches/Patch_OfficeShelf.cs
@@ -34,8 +34,11 @@ namespace DDSS_LobbyGuard.Patches
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(OfficeShelf), nameof(OfficeShelf.InvokeUserCode_CmdSpawnDocument__String__String__DocumentCategory__Int32__NetworkConnectionToClient))]
-        private static bool InvokeUserCode_CmdSpawnDocument__String__String__DocumentCategory__Int32__NetworkConnectionToClient_Prefix(NetworkReader __1, NetworkConnectionToClient __2)
+        private static bool InvokeUserCode_CmdSpawnDocument__String__String__DocumentCategory__Int32__NetworkConnectionToClient_Prefix(NetworkBehaviour __0, NetworkReader __1, NetworkConnectionToClient __2)
         {
+            // Get OfficeShelf
+            OfficeShelf shelf = __0.TryCast<OfficeShelf>();
+
             // Get Document
             string document = __1.SafeReadString();
             if (string.IsNullOrEmpty(document)
@@ -49,12 +52,13 @@ namespace DDSS_LobbyGuard.Patches
                 return false;
 
             // Get Document Category
-            DocumentCategory documentCategory = (DocumentCategory)__1.SafeReadInt();
+            int docCatId = __1.SafeReadInt();
+            DocumentCategory documentCategory = (DocumentCategory)docCatId;
 
             // Get Binder
             int binder = __1.SafeReadInt();
             if (binder < 0)
-                return false;
+                binder = 0;
 
             // Validate Binder Manager
             if ((BinderManager.instance == null)
@@ -64,14 +68,9 @@ namespace DDSS_LobbyGuard.Patches
 
             // Get Binder Category
             var binderCat = BinderManager.instance.binders[documentCategory];
-            if ((binderCat == null)
-                || (binderCat.Count <= 0))
-                return false;
-
-            // Validate Binder
             int binderCount = binderCat.Count - 1;
             if (binder > binderCount)
-                return false;
+                binder = binderCount;
 
             // Get Binder
             Binder binderObj = binderCat[binder];
@@ -94,7 +93,7 @@ namespace DDSS_LobbyGuard.Patches
                 return false;
 
             // Run Game Command
-            binderObj.ServerAddDocument(document, documentContent);
+            shelf.UserCode_CmdSpawnDocument__String__String__DocumentCategory__Int32__NetworkConnectionToClient(document, fileName, documentCategory, binder, __2);
 
             // Prevent Original
             return false;
