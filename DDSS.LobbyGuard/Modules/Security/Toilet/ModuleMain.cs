69b35ca9ce8a3a0fe391e6058b79b6939c7ea92e
Herp Derpinstine <herpderpinstine@gmail.com>
2025-02-19 22:20:48 -0600

    Implemented Toilet Security module

---
+++ DDSS.LobbyGuard/Modules/Security/Toilet/ModuleMain.cs
@@ -0,0 +1,7 @@
+﻿namespace DDSS_LobbyGuard.Modules.Security.Toilet
+{
+    internal class ModuleMain : ILobbyModule
+    {
+        public override string Name => "Security.Toilet";
+    }
+}
---
+++ DDSS.LobbyGuard/Modules/Security/Toilet/Patches/Patch_Toilet.cs
@@ -0,0 +1,37 @@
+﻿using DDSS_LobbyGuard.Utils;
+using HarmonyLib;
+using Il2CppMirror;
+
+namespace DDSS_LobbyGuard.Modules.Security.Toilet.Patches
+{
+    [LobbyModulePatch(typeof(ModuleMain))]
+    internal class Patch_Toilet
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Il2CppProps.WC.Toilet.Toilet), nameof(Il2CppProps.WC.Toilet.Toilet.InvokeUserCode_CmdShit__NetworkIdentity__NetworkConnectionToClient))]
+        private static bool InvokeUserCode_CmdShit__NetworkIdentity__NetworkConnectionToClient_Prefix(
+            NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get Toilet
+            Il2CppProps.WC.Toilet.Toilet toilet = __0.TryCast<Il2CppProps.WC.Toilet.Toilet>();
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Seat
+            if (toilet.usingPlayerController != sender)
+                return false;
+
+            // Validate Role
+            if (sender.IsGhost())
+                return false;
+
+            // Run Game Command
+            toilet.UserCode_CmdShit__NetworkIdentity__NetworkConnectionToClient(sender, sender.connectionToClient);
+
+            // Prevent Original
+            return false;
+        }
+    }
+}
