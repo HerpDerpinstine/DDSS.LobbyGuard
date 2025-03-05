345d7a101d07461e07a7fb3e34a72f104419a6a3
Herp Derpinstine <herpderpinstine@gmail.com>
2025-03-05 01:41:22 -0600

    Fixed VerifySteamId NRE Error

---
+++ DDSS.LobbyGuard/Modules/Fixes/NoVerifyError/ModuleMain.cs
@@ -0,0 +1,7 @@
+﻿namespace DDSS_LobbyGuard.Modules.Fixes.NoVerifyError
+{
+    internal class ModuleMain : ILobbyModule
+    {
+        public override string Name => "Fixes.NoVerifyError";
+    }
+}
---
+++ DDSS.LobbyGuard/Modules/Fixes/NoVerifyError/Patches/Patch_LobbyPlayer.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using Il2CppMirror;
+using Il2CppPlayer.Lobby;
+
+namespace DDSS_LobbyGuard.Modules.Fixes.NoVerifyError.Patches
+{
+    [LobbyModulePatch(typeof(ModuleMain))]
+    internal class Patch_LobbyPlayer
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(LobbyPlayer), nameof(LobbyPlayer.VerifySteamId))]
+        private static bool VerifySteamId_Prefix(LobbyPlayer __instance)
+        {
+            // Check for Host
+            if (!NetworkServer.activeHost
+                || __instance == null
+                || __instance.WasCollected
+                || __instance.connectionToClient == null
+                || __instance.connectionToClient.WasCollected
+                || !__instance.connectionToClient.isReady
+                || !__instance.connectionToClient.isAuthenticated)
+                return false;
+
+            return true;
+        }
+    }
+}
