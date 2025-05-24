35956b95db2bc7fa40c01c47b857ba4bc07d16da
Herp Derpinstine <herpderpinstine@gmail.com>
2025-05-24 09:56:42 -0600

    Reimplemented Pumpkin Security

---
+++ DDSS.LobbyGuard/Modules/Security/Pumpkin/ModuleMain.cs
@@ -0,0 +1,7 @@
+﻿namespace DDSS_LobbyGuard.Modules.Security.Pumpkin
+{
+    internal class ModuleMain : ILobbyModule
+    {
+        public override string Name => "Security.Pumpkin";
+    }
+}
---
+++ DDSS.LobbyGuard/Modules/Security/Pumpkin/Patches/Patch_PumpkinController.cs
@@ -0,0 +1,40 @@
+﻿using DDSS_LobbyGuard.SecurityExtension;
+using DDSS_LobbyGuard.Utils;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppMirror;
+
+namespace DDSS_LobbyGuard.Modules.Security.Pumpkin.Patches
+{
+    [LobbyModulePatch(typeof(ModuleMain))]
+    internal class Patch_PumpkinController
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(PumpkinController), nameof(PumpkinController.InvokeUserCode_CmdSetLit__NetworkIdentity__Boolean__NetworkConnectionToClient))]
+        private static bool InvokeUserCode_CmdSetLit__NetworkIdentity__Boolean__NetworkConnectionToClient_Prefix(
+             NetworkBehaviour __0,
+             NetworkReader __1,
+             NetworkConnectionToClient __2)
+        {
+            // Get PumpkinController
+            PumpkinController pumpkin = __0.TryCast<PumpkinController>();
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (sender.IsGhost()
+                || !InteractionSecurity.IsWithinRange(sender.transform.position, pumpkin.transform.position))
+                return false;
+
+            __1.SafeReadNetworkIdentity();
+            bool shouldLight = __1.SafeReadBool();
+
+            // Run Game Command
+            pumpkin.UserCode_CmdSetLit__NetworkIdentity__Boolean__NetworkConnectionToClient(sender, shouldLight, sender.connectionToClient);
+
+            // Prevent Original
+            return false;
+        }
+    }
+}
