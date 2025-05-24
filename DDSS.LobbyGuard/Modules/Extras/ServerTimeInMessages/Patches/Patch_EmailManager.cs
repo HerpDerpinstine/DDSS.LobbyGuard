7b99bd99a3c56f962c36d548fa568fef71a072f3
Herp Derpinstine <herpderpinstine@gmail.com>
2025-05-24 07:22:13 -0600

    Reimplemented Email Security

---
+++ DDSS.LobbyGuard/Modules/Extras/ServerTimeInMessages/Patches/Patch_EmailManager.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using System;
+
+namespace DDSS_LobbyGuard.Modules.Extras.ServerTimeInMessages.Patches
+{
+    [LobbyModulePatch(typeof(ModuleMain))]
+    internal class Patch_EmailManager
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(EmailManager), nameof(EmailManager.UserCode_CmdSendEmail__String__String__String__String__String))]
+        private static void UserCode_CmdSendEmail__String__String__String__String__String_Prefix(ref string __4)
+        {
+            if (!ModuleConfig.Instance.UseServerTimeStampForChatMessages.Value)
+                return;
+            __4 = DateTime.Now.ToString("HH:mm");
+        }
+    }
+}
