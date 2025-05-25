fec194a802d9bf16af752ab52a73bdd8d13dc457
Herp Derpinstine <herpderpinstine@gmail.com>
2025-05-25 01:24:58 -0600

    Reimplemented Window Bounds Clamp

---
+++ DDSS.LobbyGuard/Modules/Extras/WindowBoundsClamp/ModuleMain.cs
@@ -0,0 +1,7 @@
+﻿namespace DDSS_LobbyGuard.Modules.Extras.WindowBoundsClamp
+{
+    internal class ModuleMain : ILobbyModule
+    {
+        public override string Name => "Extras.WindowBoundsClamp";
+    }
+}
