4ffe550c39f27a497326830326b5a51cf79eba01
Herp Derpinstine <herpderpinstine@gmail.com>
2025-05-25 03:44:24 -0600

    More Collectible Security

---
+++ DDSS.LobbyGuard/Modules/Security/Collectible/Patches/Patch_BeerController.cs
@@ -0,0 +1,53 @@
+﻿using DDSS_LobbyGuard.SecurityExtension;
+using DDSS_LobbyGuard.Utils;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppInterop.Runtime;
+using Il2CppMirror;
+
+namespace DDSS_LobbyGuard.Modules.Security.Collectible.Patches
+{
+    [LobbyModulePatch(typeof(ModuleMain))]
+    internal class Patch_BeerController
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(BeerController), nameof(BeerController.InvokeUserCode_CmdDrinkBeer__NetworkIdentity__NetworkConnectionToClient))]
+        private static bool InvokeUserCode_CmdDrinkBeer__NetworkIdentity__NetworkConnectionToClient_Prefix(
+            NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get BeerController
+            BeerController beer = __0.TryCast<BeerController>();
+            if ((beer == null)
+                || beer.NetworkisEmpty)
+                return false;
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+            if (sender.IsGhost())
+                return false;
+
+            // Validate Distance
+            if (!InteractionSecurity.IsWithinRange(sender.transform.position, beer.transform.position))
+                return false;
+
+            // Validate Placement
+            Il2CppProps.Scripts.Collectible collectible = sender.GetCurrentCollectible();
+            if ((collectible == null)
+                || (collectible.GetIl2CppType() != Il2CppType.Of<BeerController>()))
+                return false;
+
+            // Get Document
+            beer = collectible.TryCast<BeerController>();
+            if ((beer == null)
+                || beer.NetworkisEmpty)
+                return false;
+
+            // Run Game Command
+            beer.UserCode_CmdDrinkBeer__NetworkIdentity__NetworkConnectionToClient(sender, __2);
+
+            // Prevent Original
+            return false;
+        }
+    }
+}
---
+++ DDSS.LobbyGuard/Modules/Security/Collectible/Patches/Patch_FireExHolder.cs
@@ -0,0 +1,28 @@
+﻿using DDSS_LobbyGuard.SecurityExtension;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppMirror;
+
+namespace DDSS_LobbyGuard.Modules.Security.Collectible.Patches
+{
+    [LobbyModulePatch(typeof(ModuleMain))]
+    internal class Patch_FireExHolder
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(FireExHolder), nameof(FireExHolder.Start))]
+        private static bool Start_Prefix(FireExHolder __instance)
+        {
+            // Check for Server
+            if (!__instance.isServer)
+                return false;
+            if (!NetworkServer.activeHost)
+                return false;
+
+            // Fire Extinguisher Security
+            CollectibleSecurity.SpawnFireEx(__instance);
+
+            // Prevent Original
+            return false;
+        }
+    }
+}
