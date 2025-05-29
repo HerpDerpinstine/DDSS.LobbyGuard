c787bbe3e42e1b99f185d3c41b50f070bc91b2c4
Herp Derpinstine <herpderpinstine@gmail.com>
2025-05-29 14:06:23 -0600

    Update ModuleMain.cs

--- DDSS.LobbyGuard/Modules/Security/Object/ModuleMain.cs
+++ DDSS.LobbyGuard/Modules/Security/Object/ModuleMain.cs
@@ -1,4 +1,8 @@
-﻿namespace DDSS_LobbyGuard.Modules.Security.Object
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DDSS_LobbyGuard.Modules.Security.Object
 {
     internal class ModuleMain : ILobbyModule
     {
@@ -7,15 +11,75 @@
 
         public override void OnSceneInit(int buildIndex, string sceneName)
         {
-            // Get Scene
+            var scene = SceneManager.GetActiveScene();
 
-            // Find All Colliders in Scene
+            var rootObjs = scene.GetRootGameObjects();
+            List<Collider> allColliders = new();
+            foreach (var obj in rootObjs)
+                FindAllColliders(ref allColliders, obj);
 
-            // Get Position + Offset of Collider
+            Vector3 minPos = new();
+            Vector3 maxPos = new();
+            foreach (var collider in allColliders)
+            {
+                Vector3 colliderPosition = collider.transform.position;
+                CompareMinMaxVec(ref minPos, colliderPosition, true);
+                CompareMinMaxVec(ref maxPos, colliderPosition, false);
 
-            // Get Collider Bounds
+                var colliderBounds = collider.bounds;
 
-            // Cache Min/Max
+            }
+        }
+
+        private static void CompareMinMaxVec(ref Vector3 input,
+            Vector3 targetPos,
+            bool lessThan)
+        {
+            if (lessThan)
+            {
+                if (targetPos.x < input.x)
+                    input.x = targetPos.x;
+                if (targetPos.y < input.y)
+                    input.y = targetPos.y;
+                if (targetPos.z < input.z)
+                    input.z = targetPos.z;
+            }
+            else
+            {
+                if (targetPos.x > input.x)
+                    input.x = targetPos.x;
+                if (targetPos.y > input.y)
+                    input.y = targetPos.y;
+                if (targetPos.z > input.z)
+                    input.z = targetPos.z;
+            }
+        }
+
+        private static void FindAllColliders(ref List<Collider> allColliders, GameObject target)
+        {
+            if ((target == null)
+                || target.WasCollected)
+                return;
+
+            Collider[] foundColliders = target.GetComponents<Collider>();
+            if (foundColliders.Length > 0)
+                foreach (var collider in foundColliders)
+                {
+                    if ((collider == null)
+                        || collider.WasCollected)
+                        continue;
+                    allColliders.Add(collider);
+                }
+
+            int childCount = target.transform.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                Transform child = target.transform.GetChild(i);
+                if ((child == null)
+                    || child.WasCollected)
+                    continue;
+                FindAllColliders(ref allColliders, child.gameObject);
+            }
         }
     }
 }
