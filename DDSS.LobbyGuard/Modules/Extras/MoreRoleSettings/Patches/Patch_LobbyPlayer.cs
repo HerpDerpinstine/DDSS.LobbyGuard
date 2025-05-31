12fa8fda54e7a2eb45f6e91ebce5f33f8a4419e8
Herp Derpinstine <herpderpinstine@gmail.com>
2025-05-31 10:57:37 -0600

    Improved Player Security

---
+++ DDSS.LobbyGuard/Modules/Extras/MoreRoleSettings/Patches/Patch_LobbyPlayer.cs
@@ -0,0 +1,47 @@
+﻿using DDSS_LobbyGuard.Utils;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppGameManagement;
+using Il2CppMirror;
+using Il2CppPlayer.Lobby;
+
+namespace DDSS_LobbyGuard.Modules.Extras.MoreRoleSettings.Patches
+{
+    [LobbyModulePatch(typeof(ModuleMain))]
+    internal class Patch_LobbyPlayer
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(LobbyPlayer), nameof(LobbyPlayer.DeserializeSyncVars))]
+        private static void DeserializeSyncVars_Prefix(LobbyPlayer __instance)
+            => EnforcePlayerValues(__instance);
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(LobbyPlayer), nameof(LobbyPlayer.DeserializeSyncVars))]
+        private static void DeserializeSyncVars_Postfix(LobbyPlayer __instance)
+            => EnforcePlayerValues(__instance);
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(LobbyPlayer), nameof(LobbyPlayer.SerializeSyncVars))]
+        private static void SerializeSyncVars_Prefix(LobbyPlayer __instance)
+            => EnforcePlayerValues(__instance);
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(LobbyPlayer), nameof(LobbyPlayer.SerializeSyncVars))]
+        private static void SerializeSyncVars_Postfix(LobbyPlayer __instance)
+            => EnforcePlayerValues(__instance);
+
+        private static void EnforcePlayerValues(LobbyPlayer __instance)
+        {
+            if (!NetworkServer.activeHost
+                || (GameManager.instance == null)
+                || GameManager.instance.WasCollected)
+                return;
+
+            // Check if Win Screen is Hidden
+            if (GameManager.instance.GetWinner() == PlayerRole.None)
+            {
+                // Force NetworkisFired to false for Janitors to be Reassignable
+                if (ModuleConfig.Instance.AllowJanitorsToKeepWorkStation.Value
+                    && __instance.IsJanitor())
+                    __instance.isFired = false;
+            }
+        }
+    }
+}
--- DDSS.LobbyGuard/Modules/Security/Player/Patches/Patch_LobbyPlayer.cs
+++ DDSS.LobbyGuard/Modules/Security/Player/Patches/Patch_LobbyPlayer.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using Il2Cpp;
 using Il2CppInterop.Runtime;
 using Il2CppMirror;
 using Il2CppPlayer;
@@ -67,6 +68,32 @@ namespace DDSS_LobbyGuard.Modules.Security.Player.Patches
 
                 usable.UserCode_CmdStopUse__NetworkIdentity__NetworkConnectionToClient(controllerNet, controller.connectionToClient);
             }
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(LobbyPlayer), nameof(LobbyPlayer.InvokeUserCode_CmdReplacePlayer__NetworkConnectionToClient))]
+        private static bool InvokeUserCode_CmdReplacePlayer__NetworkConnectionToClient_Prefix(NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Validate Server
+            if ((__2.identity == null)
+                || __2.identity.WasCollected
+                || !LobbyManager.instance.gameStarted)
+                return false;
+
+            // Validate Sender
+            LobbyPlayer sender = __0.TryCast<LobbyPlayer>();
+            if ((sender == null)
+                || sender.WasCollected
+                || ((sender.NetworkplayerController != null)
+                    && !sender.NetworkplayerController.WasCollected))
+                return false;
+
+            // Run Game Command
+            sender.UserCode_CmdReplacePlayer__NetworkConnectionToClient(__2);
+
+            // Prevent Original
+            return false;
         }
     }
 }
