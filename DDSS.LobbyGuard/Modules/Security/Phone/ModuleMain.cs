354353a4efb010717f5a9e9f89ee683b9fe69f18
Herp Derpinstine <herpderpinstine@gmail.com>
2025-05-31 14:09:44 -0600

    Reimplemented Phone Security Module

---
+++ DDSS.LobbyGuard/Modules/Security/Phone/ModuleMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDSS_LobbyGuard.Modules.Security.Phone
+{
+    internal class ModuleMain : ILobbyModule
+    {
+        public override string Name => "Phone";
+        public override eModuleType ModuleType => eModuleType.Security;
+
+        public override bool OnLoad() => false;
+    }
+}
