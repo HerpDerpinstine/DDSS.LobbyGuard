bc32387af00401cdbd3caee869d9f95ef675bb05
Herp Derpinstine <herpderpinstine@gmail.com>
2025-06-01 10:43:16 -0600

    Create Patch_Collectible.cs

---
+++ DDSS.LobbyGuard/Modules/Security/Object/Patches/Patch_Collectible.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Il2CppProps.Scripts;
+
+namespace DDSS_LobbyGuard.Modules.Security.Object.Patches
+{
+    [LobbyModulePatch(typeof(ModuleMain))]
+    internal class Patch_Collectible
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Collectible), nameof(Collectible.Start))]
+        private static void Start_Postfix(Collectible __instance)
+        {
+            __instance.despawnAfterIdle = ModuleConfig.Instance.DespawnIdleCollectibles.Value;
+        }
+    }
+}
