2c11ca937ba398cd1f131fcde268a19ea199d7ad
Herp Derpinstine <herpderpinstine@gmail.com>
2025-06-03 12:30:51 -0600

    Fixed an issue with Filing Cabinets not Organizing properly

--- DDSS.LobbyGuard/Modules/Security/Shelf/Patches/Patch_DrawerController.cs
+++ DDSS.LobbyGuard/Modules/Security/Shelf/Patches/Patch_DrawerController.cs
@@ -3,7 +3,6 @@ using DDSS_LobbyGuard.Utils;
 using HarmonyLib;
 using Il2Cpp;
 using Il2CppMirror;
-using Il2CppPlayer.Tasks;
 
 namespace DDSS_LobbyGuard.Modules.Security.Shelf.Patches
 {
@@ -19,11 +18,6 @@ namespace DDSS_LobbyGuard.Modules.Security.Shelf.Patches
             // Get DrawerController
             DrawerController drawer = __0.TryCast<DrawerController>();
 
-            // Check if Drawer is Open and Unorganized
-            if (!drawer.NetworkisOpen
-                || drawer.NetworkisOrganized)
-                return false;
-
             // Get Sender
             NetworkIdentity sender = __2.identity;
 
@@ -34,26 +28,6 @@ namespace DDSS_LobbyGuard.Modules.Security.Shelf.Patches
 
             // Run Game Command
             drawer.UserCode_CmdOrganize__NetworkIdentity__NetworkConnectionToClient(sender, __2);
-            drawer.filingCabinetController.RpcCalculateIsOrganized(sender);
-
-            // Manually Trigger Task Locally
-            if (sender.isLocalPlayer)
-            {
-                // Check if Every Drawer has been Organized
-                bool isAllOrganized = false;
-                foreach (DrawerController drawerController in drawer.filingCabinetController.drawers)
-                {
-                    if (!drawerController.NetworkisOrganized)
-                    {
-                        isAllOrganized = false;
-                        break;
-                    }
-                    else
-                        isAllOrganized = true;
-                }
-                if (isAllOrganized)
-                    TaskHook.TriggerTaskHookCommandStatic(new TaskHook(null, "Filing Cabinet", null, "Organized", drawer.filingCabinetController.roomTrigger.currentRoom.roomName, null));
-            }
 
             // Prevent Original
             return false;
@@ -80,42 +54,9 @@ namespace DDSS_LobbyGuard.Modules.Security.Shelf.Patches
             // Get State
             __1.SafeReadNetworkIdentity();
             bool requestedState = __1.SafeReadBool();
-            if (drawer.NetworkisOpen == requestedState)
-                return false;
 
-            // Apply State
-            drawer.NetworkisOpen = requestedState;
-            drawer.RpcSetDrawerState(sender, requestedState);
-            if (sender.isLocalPlayer)
-                drawer.filingCabinetController.RpcCalculateIsOrganized(sender);
-
-            // Reset Organization for Entire Cabinet
-            if (requestedState)
-            {
-                // Check if Every Drawer has been Organized
-                bool isAllOrganized = false;
-                foreach (DrawerController drawerController in drawer.filingCabinetController.drawers)
-                {
-                    if (!drawerController.NetworkisOrganized)
-                    {
-                        isAllOrganized = false;
-                        break;
-                    }
-                    else
-                        isAllOrganized = true;
-                }
-
-                // Unorganize Drawers
-                if (isAllOrganized)
-                {
-                    drawer.filingCabinetController.UserCode_CmdSetUnorganized__NetworkConnectionToClient(__2);
-                    if (sender.isLocalPlayer)
-                    {
-                        drawer.filingCabinetController.RpcCalculateIsOrganized(sender);
-                        TaskHook.TriggerTaskHookCommandStatic(new TaskHook(null, "Filing Cabinet", null, "Organized", drawer.filingCabinetController.roomTrigger.currentRoom.roomName, null));
-                    }
-                }
-            }
+            // Run Game Command
+            drawer.UserCode_CmdSetDrawerState__NetworkIdentity__Boolean__NetworkConnectionToClient(sender, requestedState, __2);
 
             // Prevent Original
             return false;
