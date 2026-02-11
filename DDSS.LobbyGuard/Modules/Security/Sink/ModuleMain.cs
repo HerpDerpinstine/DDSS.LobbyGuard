947b17667ab7cc8aef9a326e03f32ab8e266a399
Herp Derpinstine <herpderpinstine@gmail.com>
2026-02-11 12:59:53 -0600

    Implemented Sink Security Module

---
+++ DDSS.LobbyGuard/Modules/Security/Sink/ModuleMain.cs
@@ -0,0 +1,8 @@
+﻿namespace DDSS_LobbyGuard.Modules.Security.Sink
+{
+    internal class ModuleMain : ILobbyModule
+    {
+        public override string Name => "Sink";
+        public override eModuleType ModuleType => eModuleType.Security;
+    }
+}
---
+++ DDSS.LobbyGuard/Modules/Security/Sink/Patches/Patch_SinkController.cs
@@ -0,0 +1,36 @@
+﻿using DDSS_LobbyGuard.SecurityExtension;
+using DDSS_LobbyGuard.Utils;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppMirror;
+
+namespace DDSS_LobbyGuard.Modules.Security.Sink.Patches
+{
+    [LobbyModulePatch(typeof(ModuleMain))]
+    internal class Patch_SinkController
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(SinkController), nameof(SinkController.InvokeUserCode_CmdStartWashingHands__NetworkIdentity))]
+        private static bool InvokeUserCode_CmdStartWashingHands__NetworkIdentity_Prefix(
+            NetworkBehaviour __0,
+            NetworkConnectionToClient __2)
+        {
+            // Get SinkController
+            SinkController sink = __0.TryCast<SinkController>();
+
+            // Get Sender
+            NetworkIdentity sender = __2.identity;
+
+            // Validate Distance
+            if (sender.IsGhost()
+                || !InteractionSecurity.IsPlayerWithinInteractRange(sender, sink))
+                return false;
+
+            // Run Game Command
+            sink.UserCode_CmdStartWashingHands__NetworkIdentity(sender);
+
+            // Prevent Original
+            return false;
+        }
+    }
+}
